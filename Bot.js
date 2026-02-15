0cf4be83b0d721be41ac86b461c83e3b245a17a2
reportJNG <hamzaremali10@gmail.com>
2026-02-15 13:09:29 -0800

    upgraded logic

--- Bot.js
+++ Bot.js
@@ -1,24 +1,24 @@
-//#logic
-//we need const length  of valid
-//emptey to used number max is 3 then 2  (3 times) then 1  infinity then =>{ create.conter((1|3))=>{firstnum,secondnum}}
-
 //#functions
 //main
 //get one random num from [0-9]
 //build a array of random numbers
-//check the new array created
-//return true or repeat after checking
 
-const lengthcard = 16;
-let i = 0;
-
-const checker = (array) => {
-  let tested = [];
-
-  for (let i = 0; i < array.length; i++) {
-    tested.push[array[i]];
-  }
-};
 const main = () => {
-  let random_number = Math.floor(Math.random() * 10);
+  const lengthcard = 15;
+  let newarr = "";
+  for (let i = 0; i < lengthcard; i++) {
+    let random_number = Math.floor(Math.random() * 10);
+    newarr += random_number.toString();
+  }
+  return {
+    valid: newarr,
+  };
+};
+
+const howmuch = (n) => {
+  let j = 0;
+  while (j < n) {
+    console.log(main());
+  }
+  j++;
 };
